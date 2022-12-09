53b693eb54c074c8a657042caea6bf0b5989e9d5
Toromong <110834052+Toromong@users.noreply.github.com>
2022-12-09 21:30:48 +0900

    111
    111

--- homework/src/main/java/com/example/homework/config/WebSecurityConfig.java
+++ homework/src/main/java/com/example/homework/config/WebSecurityConfig.java
@@ -1,10 +1,11 @@
 package com.example.homework.config;
 
-
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +13,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 사용 및 resources 접근 허용 설정
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // CSRF 설정
         http.csrf().disable();
 
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests()
+
+                .anyRequest().authenticated();
 
         // 로그인 사용
         http.formLogin();
