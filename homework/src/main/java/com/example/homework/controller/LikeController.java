04bf400ec1f96408a9faa7b8436accd063bea9f2
Toromong <110834052+Toromong@users.noreply.github.com>
2022-12-16 14:40:31 +0900

    진짜진짜끝

--- homework/src/main/java/com/example/homework/controller/LikeController.java
+++ homework/src/main/java/com/example/homework/controller/LikeController.java
@@ -5,10 +5,7 @@ import com.example.homework.security.UserDetailsImpl;
 import com.example.homework.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,12 +15,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LikeController {
     private final LikeService likeService;
 
-    @RequestMapping(value = "/postGood/{articleId}", method = RequestMethod.POST)
+    @PostMapping(value = "/postGood/{articleId}")
     public ResponseDto likeArticle(@PathVariable Long articleId, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return likeService.likeArticle(articleId,userDetails.getUser());
     }
 
-    @RequestMapping(value = "/commentGood/{commentId}", method = RequestMethod.POST)
+    @PostMapping(value = "/commentGood/{commentId}")
     public ResponseDto likeComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails ){
         return likeService.likeComment(commentId, userDetails.getUser());
     }
